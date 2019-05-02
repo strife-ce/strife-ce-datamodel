c4dc76829d82bef7ef7f2f2f5724b2435d6eae3a
Patrick Schäfer <git@devsheep.de>
2019-05-02 20:15:25 +0200

    multiple changes for client

--- models/transient/chat-account.ts
+++ models/transient/chat-account.ts
@@ -1,42 +1,49 @@
 import { Account } from '../account';
-export class ChatAccount {
-    private _id: string;
-    private _name: string;
+import { TransientBaseModel } from './base/transient-base-model';
 
-    public constructor(account: Account) {
-        this.id = account.id;
-        this.name = account.name;
-    }
-    /**
-     * Getter id
-     * @return {string}
-     */
-    public get id(): string {
-        return this._id;
-    }
+export class ChatAccount extends TransientBaseModel {
+  public static CLASSNAME = 'ChatAccount';
+  private _id: string;
+  private _name: string;
 
-    /**
-     * Getter name
-     * @return {string}
-     */
-    public get name(): string {
-        return this._name;
+  public constructor(account?: Account) {
+    super(ChatAccount.CLASSNAME);
+    if (account) {
+      this.id = account.id;
+      this.name = account.name;
     }
+  }
+  /**
+   * Getter id
+   * @return {string}
+   */
+  public get id(): string {
+    return this._id;
+  }
 
-    /**
-     * Setter id
-     * @param {string} value
-     */
-    public set id(value: string) {
-        this._id = value;
-    }
+  /**
+   * Getter name
+   * @return {string}
+   */
+  public get name(): string {
+    return this._name;
+  }
 
-    /**
-     * Setter name
-     * @param {string} value
-     */
-    public set name(value: string) {
-        this._name = value;
-    }
+  /**
+   * Setter id
+   * @param {string} value
+   */
+  public set id(value: string) {
+    this._id = value;
+  }
+
+  /**
+   * Setter name
+   * @param {string} value
+   */
+  public set name(value: string) {
+    this._name = value;
+  }
 
 }
+TransientBaseModel.registerClass(ChatAccount, ChatAccount.CLASSNAME);
---
+++ models/transient/game.enums.ts
@@ -0,0 +1,85 @@
+export enum EHeroEnum {
+  BASTION,
+  CLAUDESSA,
+  FETTERSTONE,
+  LADY_TINDER,
+  NIKOLAI,
+  ROOK,
+  BO,
+  CAPRICE,
+  GOKONG,
+  RAY,
+  TRIXIE,
+  VERMILLION,
+  ZAKU,
+  BLAZER,
+  CHESTER,
+  IAH,
+  MALADY,
+  MOXIE,
+  CARTER,
+  HALE,
+  SHANK,
+  ACE,
+  HARROWER,
+  JIN_SHE,
+  MINERVA,
+  VEX,
+}
+
+export const HeroEnumText = new Map<EHeroEnum, string>()
+  .set(EHeroEnum.BASTION, 'Bastion')
+  .set(EHeroEnum.CLAUDESSA, 'Claudessa')
+  .set(EHeroEnum.FETTERSTONE, 'Fetterstone')
+  .set(EHeroEnum.LADY_TINDER, 'Lady Tinder')
+  .set(EHeroEnum.NIKOLAI, 'Nikolai')
+  .set(EHeroEnum.ROOK, 'Rook')
+  .set(EHeroEnum.BO, 'Bo')
+  .set(EHeroEnum.CAPRICE, 'Caprice')
+  .set(EHeroEnum.GOKONG, 'Gokong')
+  .set(EHeroEnum.RAY, 'Ray')
+  .set(EHeroEnum.TRIXIE, 'Trixie')
+  .set(EHeroEnum.VERMILLION, 'Vermillion')
+  .set(EHeroEnum.ZAKU, 'Zaku')
+  .set(EHeroEnum.BLAZER, 'Blazer')
+  .set(EHeroEnum.CHESTER, 'Chester')
+  .set(EHeroEnum.IAH, 'Iah')
+  .set(EHeroEnum.MALADY, 'Malady')
+  .set(EHeroEnum.MOXIE, 'Moxie')
+  .set(EHeroEnum.CARTER, 'Carter')
+  .set(EHeroEnum.HALE, 'Hale')
+  .set(EHeroEnum.SHANK, 'Shank')
+  .set(EHeroEnum.ACE, 'Ace')
+  .set(EHeroEnum.HARROWER, 'Harrower')
+  .set(EHeroEnum.JIN_SHE, 'Jin She')
+  .set(EHeroEnum.MINERVA, 'Minerva')
+  .set(EHeroEnum.VEX, 'Vex');
+
+export const HeroEnumGameName = new Map<EHeroEnum, string>()
+  .set(EHeroEnum.BASTION, 'Hero_Bastion')
+  .set(EHeroEnum.CLAUDESSA, 'Hero_Claudessa')
+  .set(EHeroEnum.FETTERSTONE, 'Hero_Fetterstone')
+  .set(EHeroEnum.LADY_TINDER, 'Hero_LadyTinder')
+  .set(EHeroEnum.NIKOLAI, 'Hero_Nikolai')
+  .set(EHeroEnum.ROOK, 'Hero_Rook')
+  .set(EHeroEnum.BO, 'Hero_Bo')
+  .set(EHeroEnum.CAPRICE, 'Hero_Caprice')
+  .set(EHeroEnum.GOKONG, 'Hero_Gokong')
+  .set(EHeroEnum.RAY, 'Hero_Ray')
+  .set(EHeroEnum.TRIXIE, 'Hero_Trixie')
+  .set(EHeroEnum.VERMILLION, 'Hero_Vermillion')
+  .set(EHeroEnum.ZAKU, 'Hero_Zaku')
+  .set(EHeroEnum.BLAZER, 'Hero_Blazer')
+  .set(EHeroEnum.CHESTER, 'Hero_Chester')
+  .set(EHeroEnum.IAH, 'Hero_Iah')
+  .set(EHeroEnum.MALADY, 'Hero_Malady')
+  .set(EHeroEnum.MOXIE, 'Hero_Moxie')
+  .set(EHeroEnum.CARTER, 'Hero_Carter')
+  .set(EHeroEnum.HALE, 'Hero_Hale')
+  .set(EHeroEnum.SHANK, 'Hero_Shank')
+  .set(EHeroEnum.ACE, 'Hero_Ace')
+  .set(EHeroEnum.HARROWER, 'Hero_Harrower')
+  .set(EHeroEnum.JIN_SHE, 'Hero_JinShe')
+  .set(EHeroEnum.MINERVA, 'Hero_Minerva')
+  .set(EHeroEnum.VEX, 'Hero_Vex');
+
